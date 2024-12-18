104be9e3af4ed1bfc96ed2eee6c4d04d7ad613a4
Sravya Rani <sravya@Sravyas-MacBook-Pro.local>
2024-12-18 17:05:48 +0530

    Initial commit

---
+++ src/main/java/com/example/springboot/restwebservice/RestwebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.restwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestwebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestwebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot/restwebservice/RestwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.restwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestwebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
