6feff7c87a7dbd94c1ff733529af64ff7e46c76a
sravya <sravya.rani143@gmail.com>
2024-12-24 21:00:02 +0530

    intro method path variable

--- src/main/java/com/example/springboot/restwebservice/controller/RestApi.java
+++ src/main/java/com/example/springboot/restwebservice/controller/RestApi.java
@@ -2,6 +2,7 @@ package com.example.springboot.restwebservice.controller;
 
 import com.example.springboot.restwebservice.bean.PrintHello;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class RestApi {
     @GetMapping("/hello-world")
     public PrintHello helloWorldPrint(){
         return new PrintHello("Hello-World");
+    }
+
+    @GetMapping("/path-variable/{name}")
+    public PrintHello pathVariable(@PathVariable String name) {
+        return new PrintHello(name);
     }
 }
